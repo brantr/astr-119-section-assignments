b614e744a120cbc66a24d4610179c6382ee7ac7d
vbautista379 <43651598+vbautista379@users.noreply.github.com>
2018-10-01 10:22:46 -0700

    Delete "hello-world-firstname-lastinitial.py

--- "hello-world-firstname-lastinitial.py
+++
@@ -1,7 +0,0 @@
-Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 26 2018, 23:26:24) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "copyright", "credits" or "license()" for more information.
->>> #!/usr/bin/env python3
->>> print("Hello from [Victor Bautista].")
-Hello from [Victor Bautista].
->>> 
